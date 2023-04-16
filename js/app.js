4f80d1b26956703cab9b98aa7fb12cbecf4fe9aa
Hnin Thiri Kyaw <hninthirikyaw.171@gmail.com>
2023-04-16 20:00:00 +0630

    added forecast js

--- js/app.js
+++ js/app.js
@@ -49,7 +49,6 @@ function search(cityName) {
   let apiURL = `https://api.shecodes.io/weather/v1/forecast?query=${cityName}&key=${apiKey}&units=metric`;
   axios.get(apiURL).then(showTemperature);
 }
-search("London");
 
 function submitHandler(event) {
   event.preventDefault();
@@ -61,17 +60,40 @@ function submitHandler(event) {
 function displayCelsius(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
-  fahrenheitLink.classList.remove("active")
-  celsiusLink.classList.add("active")
+  fahrenheitLink.classList.remove("active");
+  celsiusLink.classList.add("active");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
 function displayFahrenheit(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
-  celsiusLink.classList.remove("active")
-  fahrenheitLink.classList.add("active")
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
   temperatureElement.innerHTML = Math.round((celsiusTemperature * 9) / 5 + 32);
+}
+
+function displayForecast() {
+  let forecastHtml = document.querySelector("#forecast");
+  
+  let days = ["Mon", "Tue", "Wed" ,"Thurs", "Fri", "Sat"];
+  days.forEach(function (day) {
+    forecastHtml.innerHTML =
+      forecastHtml.innerHTML +
+      `<div class="col-2">
+  <div id="weather-forecast-date">
+  ${day}
+ </div>
+ <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png" width="48" alt="">
+ <div>
+  <span id="weather-forecast-max">18°</span>
+  <span id="weather-forecast-min">12°</span>
+ </div>
+ </div>
+ `;
+  });
+ 
+  forecastHtml.innerHTML = forecastHtml.innerHTML;
 }
 
 let celsiusTemperature = null;
@@ -84,3 +106,6 @@ celsiusLink.addEventListener("click", displayCelsius);
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheit);
+
+search("London");
+displayForecast();
